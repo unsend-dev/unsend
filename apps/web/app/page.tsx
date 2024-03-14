7ee4e89e5f989f82bbdf05553565b84d53c3b823
KMKoushik <koushikmohan1996@gmail.com>
2024-03-15 09:17:00 +1100

    Initialise repo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@unsend/ui/card";
+import { Code } from "@unsend/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@unsend/ui/button";
 
 function Gradient({
   conic,
