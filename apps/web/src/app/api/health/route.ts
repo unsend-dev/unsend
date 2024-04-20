abd1f6bb5bd43b800cbdf3ee9adfad44826c339c
KMKoushik <koushikmohan1996@gmail.com>
2024-04-21 08:53:23 +1000

    Fix eslint

--- apps/web/src/app/api/health/route.ts
+++ apps/web/src/app/api/health/route.ts
@@ -1,5 +1,7 @@
 import { setupAws } from "~/server/aws/setup";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   await setupAws();
   return Response.json({ data: "Healthy" });
