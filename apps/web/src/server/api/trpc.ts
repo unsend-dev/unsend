d2a997a34b52af353de1689bb2fedb87417606d3
KMKoushik <koushikmohan1996@gmail.com>
2024-05-20 18:30:29 +1000

    Fix: Block tRPC calls if user is not beta

--- apps/web/src/server/api/trpc.ts
+++ apps/web/src/server/api/trpc.ts
@@ -96,9 +96,10 @@ export const publicProcedure = t.procedure;
  * @see https://trpc.io/docs/procedures
  */
 export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
+  if (!ctx.session || !ctx.session.user || !ctx.session.user.isBetaUser) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
+
   return next({
     ctx: {
       // infers the `session` as non-nullable
