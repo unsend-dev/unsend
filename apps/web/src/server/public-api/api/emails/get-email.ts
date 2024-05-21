b9643f41e1c0bf8eeb4791074dd1324f1da2efc7
KM Koushik <koushikmohan1996@gmail.com>
2024-05-21 20:51:53 +1000

    Add basic documentation (#14)
    * Docs MVP
    
    * Change to mintlify
    
    * Add more docs
    
    * Init docs

---
+++ apps/web/src/server/public-api/api/emails/get-email.ts
@@ -0,0 +1,85 @@
+import { createRoute, z } from "@hono/zod-openapi";
+import { PublicAPIApp } from "~/server/public-api/hono";
+import { getTeamFromToken } from "~/server/public-api/auth";
+import { sendEmail } from "~/server/service/email-service";
+import { db } from "~/server/db";
+import { EmailStatus } from "@prisma/client";
+import { UnsendApiError } from "../../api-error";
+
+const route = createRoute({
+  method: "get",
+  path: "/v1/emails/{emailId}",
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: z.object({
+            id: z.string(),
+            teamId: z.number(),
+            to: z.string(),
+            from: z.string(),
+            subject: z.string(),
+            html: z.string().nullable(),
+            text: z.string().nullable(),
+            createdAt: z.string(),
+            updatedAt: z.string(),
+            emailEvents: z.array(
+              z.object({
+                emailId: z.string(),
+                status: z.nativeEnum(EmailStatus),
+                createdAt: z.string(),
+                data: z.any().optional(),
+              })
+            ),
+          }),
+        },
+      },
+      description: "Retrieve the user",
+    },
+  },
+});
+
+function send(app: PublicAPIApp) {
+  app.openapi(route, async (c) => {
+    const team = await getTeamFromToken(c);
+
+    const emailId = c.req.param("emailId");
+
+    const email = await db.email.findUnique({
+      where: {
+        id: emailId,
+        teamId: team.id,
+      },
+      select: {
+        id: true,
+        teamId: true,
+        to: true,
+        from: true,
+        subject: true,
+        html: true,
+        text: true,
+        createdAt: true,
+        updatedAt: true,
+        emailEvents: {
+          select: {
+            emailId: true,
+            status: true,
+            createdAt: true,
+            data: true,
+          },
+        },
+      },
+    });
+
+    if (!email) {
+      throw new UnsendApiError({
+        code: "NOT_FOUND",
+        message: "Email not found",
+      });
+    }
+
+    return c.json(email);
+  });
+}
+
+export default send;
