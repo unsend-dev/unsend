8dda051c5f7fd4cfc07878a3148ade41df333140
KM Koushik <koushikmohan1996@gmail.com>
2024-05-30 20:01:20 +1000

    Add API rate limit (#23)

--- apps/web/src/server/public-api/auth.ts
+++ apps/web/src/server/public-api/auth.ts
@@ -1,26 +1,38 @@
+import TTLCache from "@isaacs/ttlcache";
 import { Context } from "hono";
 import { hashToken } from "../auth";
 import { db } from "../db";
 import { UnsendApiError } from "./api-error";
+import { env } from "~/env";
+
+const rateLimitCache = new TTLCache({
+  ttl: 1000, // 1 second
+  max: env.API_RATE_LIMIT,
+});
 
 /**
  * Gets the team from the token. Also will check if the token is valid.
  */
 export const getTeamFromToken = async (c: Context) => {
   const authHeader = c.req.header("Authorization");
+
   if (!authHeader) {
     throw new UnsendApiError({
       code: "UNAUTHORIZED",
       message: "No Authorization header provided",
     });
   }
+
   const token = authHeader.split(" ")[1];
+
   if (!token) {
     throw new UnsendApiError({
       code: "UNAUTHORIZED",
       message: "No Authorization header provided",
     });
   }
+
+  checkRateLimit(token);
 
   const hashedToken = hashToken(token);
 
@@ -54,4 +66,19 @@ export const getTeamFromToken = async (c: Context) => {
     .catch(console.error);
 
   return team;
+};
+
+const checkRateLimit = (token: string) => {
+  let rateLimit = rateLimitCache.get<number>(token);
+
+  rateLimit = rateLimit ?? 0;
+
+  if (rateLimit >= 2) {
+    throw new UnsendApiError({
+      code: "RATE_LIMITED",
+      message: `Rate limit exceeded, ${env.API_RATE_LIMIT} requests per second`,
+    });
+  }
+
+  rateLimitCache.set(token, rateLimit + 1);
 };
