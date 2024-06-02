59e7a5a4cea5bff2f7b2d603a25c07ee23ee214c
KMKoushik <koushikmohan1996@gmail.com>
2024-06-02 10:58:13 +1000

    Move to client components

--- apps/web/src/app/(dashboard)/domains/page.tsx
+++ apps/web/src/app/(dashboard)/domains/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import DomainsList from "./domain-list";
 import AddDomain from "./add-domain";
 
-export default async function DomainsPage() {
+export default function DomainsPage() {
   return (
     <div>
       <div className="flex justify-between items-center">
