2434472f63719a778538570a7266841f6fb9aa02
harshsbhat <icybhat@gmail.com>
2024-07-26 18:10:32 +0530

    Enabled Prisma Optimize

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-
+import { withOptimize } from "@prisma/extension-optimize";
 import { env } from "~/env";
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+  }).$extends(withOptimize());
 
 // eslint-disable-next-line no-undef
 const globalForPrisma = globalThis as unknown as {
