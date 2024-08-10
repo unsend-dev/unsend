a04ed8c9b179fb2730de05413caf8754b3fb4a5d
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-08-11 05:27:13 +0530

    feat: SMTP server (#47)
    * WIP: SMTP server
    
    * Card and minor changes in Server
    
    ---------
    
    Co-authored-by: KM Koushik <koushikmohan1996@gmail.com>

--- apps/web/src/app/(dashboard)/api-keys/page.tsx
+++ apps/web/src/app/(dashboard)/api-keys/page.tsx
@@ -2,15 +2,43 @@
 
 import ApiList from "./api-list";
 import AddApiKey from "./add-api-key";
+import Smtp from "./smtp";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@unsend/ui/src/tabs";
+import { useState } from 'react';
 
 export default function ApiKeysPage() {
+  const [activeTab, setActiveTab] = useState("apiKeys");
+  const disableSmtp = true;
+  const handleTabChange = (value: any) => {
+    if (value === "smtp" && disableSmtp) {
+      return;
+    }
+    setActiveTab(value);
+  };
+
   return (
     <div>
-      <div className="flex justify-between items-center">
-        <h1 className="font-bold text-lg">API Keys</h1>
-        <AddApiKey />
-      </div>
-      <ApiList />
+      <Tabs defaultValue="apiKeys" value={activeTab} onValueChange={handleTabChange}>
+        <TabsList>
+          <TabsTrigger value="apiKeys">API keys</TabsTrigger>
+          <TabsTrigger
+            value="smtp"
+            className={`cursor-pointer ${disableSmtp ? 'opacity-50 pointer-events-none' : ''}`}
+          >
+            SMTP
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="apiKeys">      
+          <div className="flex justify-between items-center">
+            <h1 className="font-bold text-lg">API Keys</h1>
+            <AddApiKey />
+          </div>
+          <ApiList />
+        </TabsContent>
+        <TabsContent value="smtp">
+          <Smtp />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
