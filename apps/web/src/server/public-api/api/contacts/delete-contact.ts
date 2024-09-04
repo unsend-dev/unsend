a603bd3ad92dbd699fda04fd6b2dd94ba08ba86c
8times4 <46720448+8times4@users.noreply.github.com>
2024-09-04 17:04:57 +0200

    add upsert and delete to contacts

---
+++ apps/web/src/server/public-api/api/contacts/delete-contact.ts
@@ -0,0 +1,53 @@
+import { createRoute, z } from "@hono/zod-openapi";
+import { PublicAPIApp } from "~/server/public-api/hono";
+import { getTeamFromToken } from "~/server/public-api/auth";
+import { deleteContact } from "~/server/service/contact-service";
+import { getContactBook } from "../../api-utils";
+
+const route = createRoute({
+    method: "delete",
+    path: "/v1/contactBooks/{contactBookId}/contacts/{contactId}",
+    request: {
+        params: z.object({
+            contactBookId: z.string().openapi({
+                param: {
+                    name: "contactBookId",
+                    in: "path",
+                },
+                example: "cuiwqdj74rygf74",
+            }),
+            contactId: z.string().openapi({
+                param: {
+                    name: "contactId",
+                    in: "path",
+                },
+                example: "cuiwqdj74rygf74",
+            }),
+        }),
+    },
+    responses: {
+        200: {
+            content: {
+                "application/json": {
+                    schema: z.object({ success: z.boolean() }),
+                },
+            },
+            description: "Contact deleted successfully",
+        },
+    },
+});
+
+function deleteContactHandler(app: PublicAPIApp) {
+    app.openapi(route, async (c) => {
+        const team = await getTeamFromToken(c);
+
+        await getContactBook(c, team.id);
+        const contactId = c.req.param("contactId");
+
+        await deleteContact(contactId);
+
+        return c.json({ success: true });
+    });
+}
+
+export default deleteContactHandler;
