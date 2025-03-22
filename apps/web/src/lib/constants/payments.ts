9c9b7616c1fadc8e6cd1df886611169ec2d4192a
KMKoushik <koushikmohan1996@gmail.com>
2025-03-23 09:32:00 +1100

    add domain copy to pricing features

--- apps/web/src/lib/constants/payments.ts
+++ apps/web/src/lib/constants/payments.ts
@@ -3,11 +3,13 @@ export const PLAN_PERKS = {
     "Send up to 3000 emails per month",
     "Send up to 100 emails per day",
     "Can have 1 contact book",
+    "Can have 1 domain",
   ],
   BASIC: [
     "Includes $10 of usage monthly",
     "Send transactional emails at $0.0004 per email",
     "Send marketing emails at $0.001 per email",
     "Can have unlimited contact books",
+    "Can have unlimited domains",
   ],
 };
