72a650c3ab3aee935926281fd4ddde367ee532b6
KMKoushik <koushikmohan1996@gmail.com>
2025-03-29 01:08:06 +1100

    fix build

--- apps/web/src/app/join-team/page.tsx
+++ apps/web/src/app/join-team/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import JoinTeam from "~/components/team/JoinTeam";
+import { Suspense } from "react";
+import Spinner from "@unsend/ui/src/spinner";
 
 export default function CreateTeam() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className=" w-[300px] flex flex-col gap-8">
-        <JoinTeam />
+        <Suspense
+          fallback={
+            <div className="flex justify-center">
+              <Spinner className="h-5 w-5" innerSvgClass="stroke-primary" />
+            </div>
+          }
+        >
+          <JoinTeam />
+        </Suspense>
       </div>
     </div>
   );
